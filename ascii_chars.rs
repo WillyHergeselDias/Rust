b15c2897e8afa48fa7ee9a11157db00cd2e890f3
Will <148807368+WillyHergeselDias@users.noreply.github.com>
2025-09-03 23:25:17 -0300

    Update ascii_chars.rs

--- ascii_chars.rs
+++ ascii_chars.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn main(){
 let mut string = String::new();
-io::stdin().read_line(&mut string).expect("Vai tomar no seu cu");
+io::stdin().read_line(&mut string).expect("Erro ao ler a linha.");
 let trim_string = string.trim();
 for i in 0..trim_string.chars().count(){
   if let Some(c) = trim_string.chars().nth(i){
